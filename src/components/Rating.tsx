b8f3ed18ab10177a5200ce0987ffd5dba3f79d6b
shinozukaoscar <shinozukaoscar@gmail.com>
2022-08-16 13:21:38 +0900

    update

--- src/components/Rating.tsx
+++ src/components/Rating.tsx
@@ -2,10 +2,13 @@ const Rating = (props: { rating: number; name: string }) => {
   return (
     <div class="rating rating-md rating-half">
       {Array.from({ length: 5 }).map((_, i) => {
-        const isHalfChecked = props.rating === (i * 2 + 1) * 0.5;
-        const isFullChecked = props.rating === (i * 2 + 2) * 0.5;
+        const isMatching = props.rating === (i * 2 + 1) * 0.5;
+        const isLess = (i * 2 + 1) * 0.5 <= props.rating; 
 
-        if (isHalfChecked) {
+        const isMatching1 = props.rating === (i * 2 + 2) * 0.5;
+        const isLess1 = (i * 2 + 2) * 0.5 <= props.rating; 
+
+        if (isMatching && isLess) {
           return (
             <>
               <input
@@ -23,7 +26,7 @@ const Rating = (props: { rating: number; name: string }) => {
           );
         }
 
-        if (isFullChecked) {
+        if (isMatching1 && isLess1) {
           return (
             <>
               <input
